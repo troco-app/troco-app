eb7147593ac2b283d94ad32a0894ac3be25e7ae4
Yorch <20454656+jorge-alejandro@users.noreply.github.com>
2023-07-18 18:32:39 +0200

    header en App

--- 2hand-ecommerce-shop-front/src/App.jsx
+++ 2hand-ecommerce-shop-front/src/App.jsx
@@ -1,10 +1,12 @@
 import "./index.css";
 import CookieConsent from "./components/CookieConsent";
 import { MainContent } from "./components/MainContent";
+import { Header } from "../src/components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <CookieConsent />
       <MainContent />
     </>
