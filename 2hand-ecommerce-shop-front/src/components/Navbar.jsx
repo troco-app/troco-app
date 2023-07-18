8f96db5bee93bf0227f27182cd0a961bfdd68819
Jorge <jorgecidrefdez@gmail.com>
2023-07-18 18:20:58 +0200

    creacion de header y modificaciones en home

--- 2hand-ecommerce-shop-front/src/components/Navbar.jsx
+++ 2hand-ecommerce-shop-front/src/components/Navbar.jsx
@@ -1,24 +1,9 @@
 import "../assets/css/Navbar.css";
-import { useState } from "react";
-import { useEffect } from "react";
 
 export function Navbar() {
-    const [showNavbar, setShowNavbar] = useState(true);
-  
-    useEffect(() => {
-      const handleResize = () => {
-        setShowNavbar(window.innerWidth < 768);
-      };
-  
-      window.addEventListener('resize', handleResize);
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }, []);
-  
-    return showNavbar ? (
+    return  (
       <nav className="navbar">
         <input type="text" placeholder="FIND SOMETHING" />
       </nav>
-    ) : null;
+    );
   }
--- 2hand-ecommerce-shop-front/src/pages/Home.jsx
+++ 2hand-ecommerce-shop-front/src/pages/Home.jsx
@@ -1,30 +1,15 @@
 import { useEffect } from "react";
-import { Navbar } from "../components/Navbar";
+import { Header } from "../components/Header";
 import { Categories } from "../components/Categories";
 import { useState } from "react";
-import { PopupHome } from "../components/PopupHome";
-import { NavbarDesktop } from "../components/NavbarDesktop";
 import { Footer } from "../components/Footer";
-import "../assets/css/Popup.css";
 //import { CookieConsent } from "../components/CookieConsent";
 import "../assets/css/Home.css";
-import logo from "../assets/img/logo.svg";
 import atari from "../assets/img/Atari-2600.jpg";
 import nintendo from "../assets/img/nintendo.jpg";
 import psp from "../assets/img/psp.jpg";
-import userhome from "../assets/img/user_white.svg";
-//Hello
+
 export function Home() {
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-    const openPopup = () => {
-        setIsPopupOpen(true);
-    };
-
-    const closePopup = () => {
-        setIsPopupOpen(false);
-    };
-
     const [currentImage, setCurrentImage] = useState(0);
     const images = [atari, nintendo, psp];
 
@@ -37,25 +22,8 @@ export function Home() {
     }, []);
     return (
         <>
-            <nav className="navPrincipal">
-                <div
-                    className="logo"
-                    style={{ backgroundImage: `url(${logo})` }}
-                ></div>
-                <NavbarDesktop />
-                <button className="popupHome" onClick={openPopup}>
-                    <i className="material-symbols-rounded ">menu</i>
-                    <img
-                        className="userhome"
-                        src={userhome}
-                        alt="Icono usuario"
-                    />
-                </button>
-                <PopupHome isOpen={isPopupOpen} onClose={closePopup} />
-            </nav>
-            <Navbar />
+            <Header />
             <Categories />
-
             <section className="pageSectionNav">
                 <div className="leftContainer">
                     <h1>Trade your treasure and</h1>
@@ -72,7 +40,6 @@ export function Home() {
                     className="sectionImage"
                 />
             </section>
-
             <Footer />
         </>
     );
