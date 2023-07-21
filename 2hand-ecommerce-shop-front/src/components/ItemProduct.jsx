6e6d9ad52e12d448138b9403711d9fdfe1285576
Jorge <jorgecidrefdez@gmail.com>
2023-07-21 20:18:22 +0200

    Item Product V2

--- 2hand-ecommerce-shop-front/src/components/ItemProduct.jsx
+++ 2hand-ecommerce-shop-front/src/components/ItemProduct.jsx
@@ -3,7 +3,7 @@ import chinon from "../assets/img/chinon.png";
 import masterSystem from "../assets/img/retro_master.png";
 import turbosquid from "../assets/img/retro_turbo.png";
 import StarRating from "./StarRating";
-
+import { FaHeart } from "react-icons/fa";
 
 export function ItemProduct() {
     return (
@@ -12,71 +12,80 @@ export function ItemProduct() {
             <div className="productContainer">
                 <article className="product">
                     <div className="textProduct">
-                    <div
-                        className="imageProduct"
-                        style={{ backgroundImage: `url(${chinon})` }}
-                    ></div>
+                        <div
+                            className="imageProduct"
+                            style={{ backgroundImage: `url(${chinon})` }}
+                        ></div>
+                        <button className="wishlist-button-product">
+                            <FaHeart className="faHeart" /> Wishlist
+                        </button>
                         <h2 className="categoriesProduct">Retro Cameras</h2>
                         <h3 className="nameProduct">Chinon Super 8</h3>
                         <p className="paragraphProduct">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. 
+                            elit.
                         </p>
                         <div className="footerProduct">
-                                <div className="valorationUser">
-                                    <p className="userName">Richard Fortus</p>
-                                    <StarRating/>
-                                </div>
-                                <div className="userButtonWrapper">
-                                    <button onClick="#">EXCHANGE</button>
-                                </div>
+                            <div className="valorationUser">
+                                <p className="userName">Richard Fortus</p>
+                                <StarRating />
                             </div>
+                            <div className="userButtonWrapper">
+                                <button onClick="#">EXCHANGE</button>
+                            </div>
+                        </div>
                     </div>
                 </article>
                 <article className="product">
                     <div className="textProduct">
-                    <div
-                        className="imageProduct"
-                        style={{ backgroundImage: `url(${masterSystem})` }}
-                    ></div>
-                        <h2 className="categoriesProduct" >Retro Consoles</h2>
-                        <h3 className="nameProduct">Sega Master System</h3>
-                        <p className="paragraphProduct">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. 
-                        </p>
-                            <div className="footerProduct">
-                                <div className="valorationUser">
-                                    <p className="userName">Duff McKagan</p>
-                                    <StarRating />
-                                </div>
-                                <div className="userButtonWrapper">
-                                    <button onClick="#">EXCHANGE</button>
-                                </div>
-                            </div>
-                    </div>
-                </article>
-                <article className="product">
-                    <div className="textProduct">
-                    <div
-                        className="imageProduct"
-                        style={{ backgroundImage: `url(${turbosquid})` }}
-                    ></div>
+                        <div
+                            className="imageProduct"
+                            style={{ backgroundImage: `url(${masterSystem})` }}
+                        ></div>
+                        <button className="wishlist-button-product">
+                            <FaHeart className="faHeart" /> Wishlist
+                        </button>
                         <h2 className="categoriesProduct">Retro Consoles</h2>
                         <h3 className="nameProduct">Sega Master System</h3>
                         <p className="paragraphProduct">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit. 
+                            elit.
                         </p>
-                            <div className="footerProduct">
-                                <div className="valorationUser">
-                                    <p className="userName">Slash</p>
-                                    <StarRating/>
-                                </div>
-                                <div className="userButtonWrapper">
-                                    <button onClick="#">EXCHANGE</button>
-                                </div>
+                        <div className="footerProduct">
+                            <div className="valorationUser">
+                                <p className="userName">Duff McKagan</p>
+                                <StarRating />
                             </div>
+                            <div className="userButtonWrapper">
+                                <button onClick="#">EXCHANGE</button>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article className="product">
+                    <div className="textProduct">
+                        <div
+                            className="imageProduct"
+                            style={{ backgroundImage: `url(${turbosquid})` }}
+                        ></div>
+                        <button className="wishlist-button-product">
+                            <FaHeart className="faHeart" /> Wishlist
+                        </button>
+                        <h2 className="categoriesProduct">Retro Consoles</h2>
+                        <h3 className="nameProduct">Sega Master System</h3>
+                        <p className="paragraphProduct">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit.
+                        </p>
+                        <div className="footerProduct">
+                            <div className="valorationUser">
+                                <p className="userName">Slash</p>
+                                <StarRating />
+                            </div>
+                            <div className="userButtonWrapper">
+                                <button onClick="#">EXCHANGE</button>
+                            </div>
+                        </div>
                     </div>
                 </article>
             </div>
