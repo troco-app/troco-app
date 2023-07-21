cf3f4c2a72aa74b2e9b41b2fba2b5500f8bd6154
Jorge <jorgecidrefdez@gmail.com>
2023-07-21 16:18:05 +0200

    Item Product and Login page Style

--- 2hand-ecommerce-shop-front/src/pages/Home.jsx
+++ 2hand-ecommerce-shop-front/src/pages/Home.jsx
@@ -8,7 +8,7 @@ import nintendo from "../assets/img/nintendo.jpg";
 import psp from "../assets/img/psp.jpg";
 import { ItemsPocket } from "../components/ItemsPocket";
 import "../assets/css/Home.css";
-//import { ItemProduct } from "../components/ItemProduct";
+import { ItemProduct } from "../components/ItemProduct";
 
 export function Home() {
   const [currentImage, setCurrentImage] = useState(0);
@@ -37,7 +37,7 @@ export function Home() {
         <img src={images[currentImage]} alt="Imagen" className="sectionImage" />
       </section>
       <ItemsPocket />
-      {/* <ItemProduct/> */}
+       <ItemProduct/> 
       <Box />
       <Footer />
     </>
